2e1f0e4f1523ae358fd4b227d1976dc3e97d637a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-06-26 12:34:03 +0000

    Update docs after building Travis build 26 of TMsangohan/pymad_hzb
    The docs were built from the branch 'master' against the commit
    bb9f9c98185705ef207094d3c158b90d25638e8c.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/TMsangohan/pymad_hzb/jobs/550780630.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post29+g342fc6e',
+    VERSION: '0.post30+gbb9f9c9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
